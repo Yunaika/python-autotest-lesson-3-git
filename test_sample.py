03fae17e1edffbaed7c9bb58ae74d343482fed0a
Julia <51692232+Yunaika@users.noreply.github.com>
2022-06-23 13:31:25 +0800

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -9,4 +9,11 @@ def test_square():
    assert num*num == 9
 
 def test_quality():
-   assert 1 != 7
+   assert 1 != 7 
+
+   
+def test_sum():
+   num_1 = 2
+   num_2 = 3
+   assert 1num_1 + num_2 == 5
+
